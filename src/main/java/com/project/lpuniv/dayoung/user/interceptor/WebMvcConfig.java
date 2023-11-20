58bc6d28c3d05cdbc55a7227802498ded5f7965b
Jlostcode <Jlostcode@github.com>
2023-11-20 16:36:57 +0900

    WebMvcConfing

--- src/main/java/com/project/lpuniv/dayoung/user/interceptor/WebMvcConfig.java
+++ src/main/java/com/project/lpuniv/dayoung/user/interceptor/WebMvcConfig.java
@@ -17,6 +17,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 //                .excludePathPatterns("/dayoung/logIn","/")
 //                .excludePathPatterns("/css/**")
 //                .excludePathPatterns("/js/**")
+//                .excludePathPatterns("//code.jquery.com/jquery-3.6.0.min.js");
 
 //    }
 }
